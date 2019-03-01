0d340daaa8b021770a52752d9a59063a9ee42faf
himanshupiplani <himanshu.piplani@mind-infotech.com>
2019-03-01 11:14:54 +0530

    parse json api code

---
+++ app/src/main/java/com/zxc/roomkotlin/jsonapi/ParseJsonApi.java
@@ -0,0 +1,94 @@
+package com.zxc.roomkotlin.jsonapi;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ParseJsonApi extends AsyncTask<String, Void, Object[]> {
+
+    public interface Response {
+        void onSuccess(String jsonString);
+
+        void onError(String errorMessage);
+    }
+
+    private Response response;
+
+    public ParseJsonApi(Response response) {
+        this.response = response;
+    }
+
+    @Override
+    protected Object[] doInBackground(String... args) {
+        try {
+            HttpURLConnection urlConnection = (HttpURLConnection) (new URL(args[0])).openConnection();
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestMethod("POST");
+
+            DataOutputStream outputStream = new DataOutputStream(urlConnection.getOutputStream());
+            outputStream.write(args[1].getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+            if (urlConnection.getResponseCode() != 200) {
+                return new Object[]{false, urlConnection.getResponseMessage()};
+            }
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String str;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((str = bufferedReader.readLine()) != null) {
+                stringBuilder.append(str);
+            }
+
+            bufferedReader.close();
+
+            return new Object[]{true, stringBuilder.toString().trim()};
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Object[]{false, e.getMessage()};
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Object[] objects) {
+        super.onPostExecute(objects);
+        if ((boolean) objects[0]) {
+            response.onSuccess((String) objects[1]);
+        } else {
+            response.onError((String) objects[1]);
+        }
+    }
+}
+
+/*
+        HOW TO USE THIS PARSE JSON API CLASS :-
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("contact", "3423432432423");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        new ParseJsonApi(new Response() {
+            @Override
+            public void onSuccess(String jsonString) {
+
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+
+            }
+        }).execute(
+                "URL",
+                jsonObject.toString().trim()
+        );
+*/
